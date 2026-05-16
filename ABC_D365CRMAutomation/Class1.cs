77faf5890285c5d3be0560c6a1a0412d7f52f992
Deepti <94973920+deepti161988@users.noreply.github.com>
2026-05-16 23:59:42 +0530

    Add files via upload

---
+++ ABC_D365CRMAutomation/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC_D365CRMAutomation
+{
+    public class Class1
+    {
+    }
+}
